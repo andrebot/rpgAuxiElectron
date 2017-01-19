aea4865e9560a4665e3ead10adf6213038b59dec
Andre Botelho <andrebot_almeida@hotmail.com>
2017-01-19 00:15:13 -0200

    Defining a better gulp task name for building the depedencies

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,12 @@
 'use strict';
 
-// const gulp = require('gulp');
+const gulp = require('gulp');
 const requireDir = require('require-dir');
-// const runSequence = require('run-sequence');
+const runSequence = require('run-sequence');
 
 // =======================
 // = Importing all tasks =
 // =======================
 requireDir('./gulp/tasks', { recurse: true });
+
+gulp.task('build', (cb) => runSequence(['copy:materialize'], cb));
