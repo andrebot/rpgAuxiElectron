7a64d1c4ae86e41e7723fe728c919df354aa8df7
Andre Botelho <andrebot_almeida@hotmail.com>
2017-01-19 02:04:52 -0200

    Adding coverage

--- gulp/tasks/test/test.js
+++ gulp/tasks/test/test.js
@@ -1,10 +1,24 @@
 'use strict';
 
 const mocha = require('gulp-mocha');
+const istanbul = require('gulp-istanbul');
 const gulp = require('gulp');
 const config = require('../../config');
 
 gulp.task('test', function test() {
   return gulp.src(config.testFiles)
           .pipe(mocha());
+});
+
+gulp.task('pre-test', function () {
+  return gulp.src(['src/**/*.js', '!src/lib/**/*', '!src/index.js'])
+          .pipe(istanbul())
+          .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test:coverage', ['pre-test'], function () {
+  return gulp.src(['test/**/*.spec.js'])
+          .pipe(mocha())
+          .pipe(istanbul.writeReports())
+          .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
 });
