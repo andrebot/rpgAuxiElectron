9018ed969cb94c6aefd3d63143d53cac6ae12e5a
Andre Botelho <andrebot_almeida@hotmail.com>
2017-01-19 01:27:46 -0200

    Added mock-require. Added more window tests

--- test/window.spec.js
+++ test/window.spec.js
@@ -1,15 +1,23 @@
 'use strict';
 
 const chai = require('chai');
-// const sinon = require('sinon');
+const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
+const mockRequire = require('mock-require');
+
+mockRequire('electron', {
+  BrowserWindow: function Brow() {
+    this.loadURL = sinon.spy();
+  }
+});
+
 const Window = require('../src/window');
 
 chai.should();
 chai.use(sinonChai);
 
-describe('Window', function windowDescribe() {
-  before(function mainBeforeEach() {
+describe('Window', function() {
+  before(function() {
     this.defaultConfig = {
       config: {
         width: 800,
@@ -21,7 +29,13 @@ describe('Window', function windowDescribe() {
     this.window = Window(this.defaultConfig);
   });
 
-  it('should have the main methods available', function mainMethods() {
+  it('should have the main methods available', function() {
     this.window.open.should.exist;
+  });
+
+  it('should be able to open itself', function() {
+    this.window.open();
+
+    this.window.view.loadURL.should.have.been.calledOnce;
   });
 });
